345500a335d3149e87810881f6eb5460a0580547
Dattabhasvant Ghadiyaram <dattabhasvan@iisc.ac.in>
2026-03-30 00:57:39 +0530

    Upgraded IsolateRoots and tested

---
+++ DyadicRealm/DyadicIntervals/Syntax.lean
@@ -0,0 +1,8 @@
+import Mathlib
+import DyadicRealm.DyadicIntervals.Basic
+import DyadicRealm.DyadicIntervals.PolynomialBounds
+import DyadicRealm.DyadicIntervals.Vectervals
+import DyadicRealm.DyadicIntervals.MvPolynomials
+import Lean
+
+syntax "poly![" term "]" : term
