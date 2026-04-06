50859b93a8564d4cfa57d6779cd639f598ae98f1
Dattabhasvant Ghadiyaram <dattabhasvan@iisc.ac.in>
2026-04-06 12:06:03 +0530

    Vecterval Repr and Dyadic Syntax

--- DyadicRealm/DyadicIntervals/Syntax.lean
+++ DyadicRealm/DyadicIntervals/Syntax.lean
@@ -4,8 +4,9 @@ import DyadicRealm.DyadicIntervals.PolynomialBounds
 import DyadicRealm.DyadicIntervals.Vectervals
 import DyadicRealm.DyadicIntervals.MvPolynomials
 import Lean
-open Lean Elab Term Meta Macro Command
 
+section PolyNomialSyntax
+open Lean Elab Term Meta Macro Command
 declare_syntax_cat mono_var
 --Identifier with an optional numerical exponent is a monomial variable
 syntax ident (" ^ " num)? : mono_var
@@ -285,3 +286,89 @@ macro_rules
 
 #eval poly[x1^2, x2^2, x3^2, -3]
 #eval poly[4 * x1, x2 ; 3 * x3 ^ 2, -1 * x2^2; x2, -5]
+
+end PolyNomialSyntax
+
+section DyadicSyntax
+open Lean Meta Elab Tactic
+
+macro "valid_endpts" : tactic => `(tactic|
+  first
+  | rw [← Dyadic.toRat_le_toRat_iff]
+    try rw[Dyadic.toRat_neg]
+    try rw [Dyadic.toRat_ofNat]
+    simp only [Rat.toRat_toDyadic, Rat.floor, Int.cast_ite]
+    ring_nf;grind
+  | grind only [Rat.toDyadic_mono]
+  | norm_cast
+  )
+
+declare_syntax_cat endpt
+syntax "dy[[" coeff "," coeff "]]" : term
+
+def getPrec (c : TSyntax `coeff) : MacroM Nat := do
+  match c with
+  | `(coeff| - $_:num / $b:num) | `(coeff| $_:num / $b:num) =>
+    let prec := b.getNat.log2
+    if b.getNat = 2 ^ prec then return prec
+    else Macro.throwErrorAt c s!"Denominator {b.getNat} is not a power of 2"
+  | `(coeff| - $_) | `(coeff| $_) =>
+    return 0
+
+def coeffToDyadicWithPrec (c : TSyntax `coeff) (prec : Nat) :
+  MacroM (TSyntax `term) := do
+  let precStx := Syntax.mkNumLit (toString prec)
+  match c with
+  | `(coeff| - $a:num / $b:num) =>  `(Rat.toDyadic (-$a / $b) $precStx)
+  | `(coeff| $a:num / $b:num)   =>  `(Rat.toDyadic ($a / $b) $precStx)
+  | `(coeff| - $a:num)          =>  `(-$a)
+  | `(coeff| $a:num)            =>  `($a)
+  | _ => Macro.throwErrorAt c "Invalid dyadic"
+
+macro_rules
+  | `(dy[[ $a:coeff , $b:coeff]]) => do
+    let precA ← getPrec a
+    let precB ← getPrec b
+    let prec := max precA precB
+    let aTerm ← coeffToDyadicWithPrec a prec
+    let bTerm ← coeffToDyadicWithPrec b prec
+    `(DyadicInterval.mk $aTerm $bTerm (by valid_endpts))
+
+end DyadicSyntax
+
+example : -1 ≤ 2 := by valid_endpts
+example : Rat.toDyadic (-1/2) 2 ≤ 2 := by valid_endpts
+example : Rat.toDyadic (-3/2) 2 ≤ -1 := by valid_endpts
+
+example : Rat.toDyadic (-3/4) 2 ≤ Rat.toDyadic (3/4) 2 := by valid_endpts
+  -- grind only [Rat.toDyadic_mono]
+
+example : Rat.toDyadic (3/2) 2 ≤ 5 := by valid_endpts
+  -- rw [← Dyadic.toRat_le_toRat_iff]
+  -- try rw[Dyadic.toRat_neg]
+  -- try rw [Dyadic.toRat_ofNat]
+  -- simp only [Rat.toRat_toDyadic, Rat.floor, Int.cast_ite]
+  -- ring_nf;grind
+
+example : -1 ≤ Rat.toDyadic (3/2) 2 := by valid_endpts
+  -- rw [← Dyadic.toRat_le_toRat_iff]
+  -- try rw[Dyadic.toRat_neg]
+  -- try rw [Dyadic.toRat_ofNat]
+  -- simp only [Rat.toRat_toDyadic, Rat.floor, Int.cast_ite]
+  -- ring_nf;grind
+
+example : -1 ≤ Rat.toDyadic (-1/2) 2 := by valid_endpts
+  -- rw [← Dyadic.toRat_le_toRat_iff]
+  -- try rw[Dyadic.toRat_neg]
+  -- try rw [Dyadic.toRat_ofNat]
+  -- simp only [Rat.toRat_toDyadic, Rat.floor, Int.cast_ite]
+  -- ring_nf;grind
+
+example : 0 ≤ Rat.toDyadic (1/2) 1 := by valid_endpts
+  -- rw [← Dyadic.toRat_le_toRat_iff]
+  -- try rw[Dyadic.toRat_neg]
+  -- try rw [Dyadic.toRat_ofNat]
+  -- simp only [Rat.toRat_toDyadic, Rat.floor, Int.cast_ite]
+  -- ring_nf;grind
+
+#eval dy[[0, 1/2]]
